62a58c28235189dead819a2f4f39144cf77ccb9e
Aj Comfort <weirdbearddev@gmail.com>
2020-10-13 15:04:50 -0400

    Add IEnumerable example scene

---
+++ Assets/_Game/Scripts/ChangeWall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeWall : MonoBehaviour
+{
+    #region Members
+    [SerializeField] private Transform _wallParent = default;
+    [SerializeField] private Material _brickWall = default;
+    [SerializeField] private Material _logWall = default;
+    #endregion Members
+
+    #region Properties
+    private Material CurrentWallMaterial => _wallParent.GetChild(0).GetComponent<MeshRenderer>().material;
+    #endregion Properties
+
+    #region Methods
+    public void UpdateWall()
+    {
+        Material newWall = GetNextMaterial();
+
+        foreach (Transform item in _wallParent)
+        {
+            item.GetComponent<MeshRenderer>().material = newWall;
+        }
+    }
+    #endregion Methods
+
+    #region Helpers
+    private Material GetNextMaterial()
+    {
+        if (CurrentWallMaterial.name.Contains("Brick"))
+        {
+            print("Switch wall to logs...");
+            return _logWall;
+        }
+        else
+        {
+            print("Switch wall to bricks...");
+            return _brickWall;
+        }
+    }
+    #endregion Helpers
+}
