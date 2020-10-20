9908d66d4dbd010b07355faf82c4965d27bc8c01
Aj Comfort <weirdbearddev@gmail.com>
2020-10-20 10:00:18 -0400

    Add goal spawner to inheritence example

---
+++ Assets/_Game/Scripts/Inheritence/GoalSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalSpawner : MonoBehaviour
+{
+    #region MonoBehavioiurs
+    void Start()
+    {
+        List<Objective> goals = new List<Objective>(5);
+        for (int i = 0; i < 5; i++)
+        {
+            goals.Add(ObjectiveFactory.CreateGoal(i + 1));
+            Debug.Log($"Goal {i + 1} - {goals[i].Description}");
+        }
+    }
+    #endregion MonoBehavioiurs
+}
